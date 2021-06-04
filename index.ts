69dbadc32e0d976edf888e392ef77fc1023af0d9
takker99 <37929109+takker99@users.noreply.github.com>
2021-06-04 21:49:15 +0900

    WIP: まだ動かないけどとりあえず保存

---
+++ index.ts
@@ -0,0 +1,29 @@
+import { parse } from "https://esm.sh/@progfay/scrapbox-parser@7.1.0";
+import {
+  getUnixTime,
+  lightFormat,
+} from "https://deno.land/x/date_fns@v2.22.1/index.js";
+import { ProjectResponse } from "./types.ts";
+
+const toLc = (title: string) =>
+  title.toLowerCase().replaceAll(" ", "_").replaceAll("/", "%2F");
+
+async function getModifiedPages(
+  project: string,
+  from: Date,
+): Promise<{ project: string; title: string; updated: Date }[]> {
+  console.log(
+    `[/${project}]Start searching for pages which are updated from ${
+      lightFormat(from, "yyyy-MM-dd")
+    }`,
+  );
+  const res = await fetch(
+    `https://scrapbox.io/api/pages/${project}?limit=1000`,
+  );
+  const { pages } = (await res.json()) as ProjectResponse;
+  return pages.map(({ title, updated }) =>
+    updated > getUnixTime(from)
+      ? [{ project, title, updated: new Date(updated) }]
+      : []
+  );
+}
---
+++ types.ts
@@ -0,0 +1,22 @@
+interface ProjectResponse {
+  projectName: string; // data取得先のproject名
+  skip: number; // parameterに渡したskipと同じ
+  limit: number; // parameterに渡したlimitと同じ
+  count: number; // projectの全ページ数 (中身のないページを除く)
+  pages: {
+    id: string;
+    title: string;
+    image: string | null;
+    descriptions: string[];
+    user: { id: string };
+    pin: number; // pinされてないときは0
+    views: number;
+    linked: number;
+    commitId: string;
+    created: number;
+    updated: number;
+    accessed: number;
+    snapshotCreated: number | null;
+    pageRank: number;
+  }[];
+}
