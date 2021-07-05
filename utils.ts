c0e71d84ef2e62da7233cfece02119203b73fbe4
takker99 <37929109+takker99@users.noreply.github.com>
2021-07-05 18:17:17 +0900

    :fix: Escape some more characters

--- utils.ts
+++ utils.ts
@@ -1,2 +1,2 @@
 export const toLc = (title: string) =>
-  title.toLowerCase().replaceAll(" ", "_").replaceAll("/", "%2F");
+  title.toLowerCase().replaceAll(" ", "_").replace(/[/?#\{}^|<>]/g, char => encodeURIComponent(char));
