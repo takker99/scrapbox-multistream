7bf20f78dfaecb1163ec7b93b4aaa65252b4945e
takker99 <37929109+takker99@users.noreply.github.com>
2021-07-06 14:42:39 +0900

    :feature: Parse tables

--- parse.ts
+++ parse.ts
@@ -1,9 +1,10 @@
 import {
   CodeBlock,
   parse,
+  Table,
 } from "https://esm.sh/@progfay/scrapbox-parser@7.1.0";
 import type { Line } from "./types.ts";
-import { jsxslack } from "https://esm.sh/jsx-slack";
+import { jsxslack, Section } from "https://esm.sh/jsx-slack";
 import { toLc } from "./utils.ts";
 
 export function sb2blocks(project: string, title: string, lines: Line[]) {
@@ -13,11 +14,13 @@ export function sb2blocks(project: string, title: string, lines: Line[]) {
   return blocks.map((block) => {
     switch (block.type) {
       case "title":
-        break;
+        return jsxslack`${block.text}`;
       case "codeBlock":
         return convertCodeBlock(project, title, block);
       case "table":
+        return convertTableBlock(project, title, block);
       case "line":
+        return jsxslack``;
     }
   });
 }
@@ -26,13 +29,28 @@ function convertCodeBlock(
   project: string,
   title: string,
   { fileName, content }: CodeBlock,
-) {
+): typeof Section {
   return jsxslack`
     <Section>
       <a href="https://scrapbox.io/api/code/${project}/${toLc(title)}/${
     toLc(fileName)
   }">${fileName}</a><br />
       <pre>${content}</pre>
+    </Section>
+  `;
+}
+
+function convertTableBlock(
+  project: string,
+  title: string,
+  { fileName, cells }: Table,
+): typeof Section {
+  return jsxslack`
+    <Section>
+      <a href="https://scrapbox.io/api/table/${project}/${toLc(title)}/${
+    toLc(fileName)
+  }.csv">${fileName}</a><br />
+    ${cells.map(cell => jsxslack`${cell.map(row => row.map(node=>node.raw).join('')).join('　')}<br />`)}
     </Section>
   `;
 }
